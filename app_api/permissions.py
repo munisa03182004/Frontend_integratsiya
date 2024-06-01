c960140def98e75e2420bc27a7e8fdb531af441e
ulugbek101 <thedevu101@gmail.com>
2024-06-01 09:48:17 +0500

    Complete UserViewSet permissions for various actions

--- app_api/permissions.py
+++ app_api/permissions.py
@@ -3,14 +3,14 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerOrSuperuser(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-
-        return request.user.is_superuser
+        return True
+        # if request.method in SAFE_METHODS:
+        #     return True
+        #
+        # if request.method == ['POST']:
+        #     return request.user.is_superuser
+        #
+        # return False
 
     def has_object_permission(self, request, view, obj):
-        pass
-        # Agar ko'rmoqchi bo'lgan profilimiz o'zimizniki bo'lsa,
-        # unda ko'ra olishimiz, o'zgartira olishimiz va o'chira olishimiz
-
-        # Istisno tariqasida superuser bo'lishi mumkin
+        return True
--- app_api/views.py
+++ app_api/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 
 from . import serializers
 from . import permissions
@@ -13,3 +15,27 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
     permission_classes = [IsAuthenticated, permissions.IsOwnerOrSuperuser]
+
+    def destroy(self, request, *args, **kwargs):
+        if not request.user.is_superuser or request.user.id != kwargs.get('pk'):
+            return Response(data={"detail": "You don't have permission to perform this action"},
+                            status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        if not request.user.is_superuser or request.user.id != kwargs.get('pk'):
+            return Response(data={"detail": "You don't have permission to perform this action"},
+                            status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        if not request.user.is_superuser or request.user.id != kwargs.get('pk'):
+            return Response(data={"detail": "You don't have permission to perform this action"},
+                            status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return Response(data={"detail": "You don't have permission to perform this action"},
+                            status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
