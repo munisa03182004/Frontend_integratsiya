b3c4a3018757888dd52d2125f3503cca7fc1ab06
ulugbek101 <thedevu101@gmail.com>
2024-06-01 08:50:34 +0500

    Add status field to UserSerializer

--- app_api/serializers.py
+++ app_api/serializers.py
@@ -1,14 +1,21 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ChoiceField
 
 User = get_user_model()
 
 
 class UserSerializer(ModelSerializer):
+    status_choices = (
+        ("user", "User"),
+        ("admin", "Admin"),
+        ("superuser", "Superuser"),
+    )
+    status = ChoiceField(choices=status_choices, write_only=True)
+
     class Meta:
         model = User
-        fields = ['email', 'first_name', 'last_name', 'username', 'password']
+        fields = ['email', 'first_name', 'last_name', 'username', 'password', 'status']
         extra_kwargs = {
             'password': {
                 'write_only': True,
@@ -24,5 +31,26 @@ class UserSerializer(ModelSerializer):
             last_name=validated_data['last_name'],
         )
         user.set_password(validated_data['password'])
+
+        if validated_data.get('status'):
+            status = validated_data.get('status')
+
+            if status == "admin":
+                user.is_staff = True
+            elif status == "superuser":
+                user.is_superuser = True
+
         user.save()
         return user
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        if instance.is_superuser:
+            representation['status'] = 'superuser'
+        elif instance.is_staff:
+            representation['status'] = 'admin'
+        else:
+            representation['status'] = 'user'
+
+        return representation
