ce7670b87dd3dc8d9b92353ff778b104034e05a1
ulugbek101 <thedevu101@gmail.com>
2024-06-06 08:32:09 +0500

    Add frontend

---
+++ index.js
@@ -0,0 +1,42 @@
+const form = document.getElementById("login-form")
+const usersContainer = document.getElementById("users-container")
+
+async function onFormSubmit(event) {
+    event.preventDefault()
+    const email = event.target.email.value
+    const password = event.target.password.value
+
+    usersContainer.textContent = "Loading ..."
+
+    const tokenResponse = await fetch("http://localhost:8000/api/v1/token/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email: email, password: password})
+    })
+
+    const data = await tokenResponse.json()
+    const accessToken = data.access
+
+    const usersResponse = await fetch("http://localhost:8000/api/v1/notes/", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + accessToken
+        }
+    })
+
+    usersContainer.textContent = ""
+
+    const usersData = await usersResponse.json()
+
+    usersData.forEach(user => {
+        usersContainer.innerHTML += `
+            <p>${note.title} - ${note.body}</p>
+        `
+    })
+
+}
+
+form.addEventListener("submit", onFormSubmit)
